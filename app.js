eced6727a0e4c480e5ed9d712610d2584f288160
Nortcele7 <shreyamregmi5@gmail.com>
2025-09-20 22:47:42 +0545

    Changes made for render

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const cookieParser = require("cookie-parser");
 const express = require("express");
 const path = require("path");
@@ -22,8 +24,6 @@ const upload = multer({dest: "public/uploads/"})
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
 })
 .then(() => console.log("MongoDB connected"))
 .catch((err) => console.error("MongoDB connection error:", err));
