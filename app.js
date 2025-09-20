c47c4ca455319fd4a99d6436c3637865d4eca376
Nortcele7 <shreyamregmi5@gmail.com>
2025-09-20 22:33:50 +0545

    Render Deployment Changes are made

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ const jwt = require("jsonwebtoken");
 
 const upload = multer({dest: "public/uploads/"})
 
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.error("MongoDB connection error:", err));
+
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
